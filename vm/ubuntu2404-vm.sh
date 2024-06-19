4e926c3ca8b4eb3813defcf425f8ce6f4c9f4644
BlueTooth <BlueTooth@users.noreply.github.com>
2024-06-19 13:23:03 +0200

    Update ubuntu2404-vm.sh

--- vm/ubuntu2404-vm.sh
+++ vm/ubuntu2404-vm.sh
@@ -379,7 +379,7 @@ FILE=$(basename $URL)
 msg_ok "Downloaded ${CL}${BL}${FILE}${CL}"
 
 #sudo apt install libguestfs-tools -y
-virt-customize -a $FILE --install qemu-guest-agent --run-command "sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+virt-customize -a $FILE --install qemu-guest-agent --run-command "sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config && rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
 qemu-img resize $FILE 20G
 
 STORAGE_TYPE=$(pvesm status -storage $STORAGE | awk 'NR>1 {print $2}')
